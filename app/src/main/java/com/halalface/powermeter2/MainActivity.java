74d827968c4b7a18cee47209039d627586d84a32
halal-face <zahin.mohammad@gmail.com>
2018-05-29 13:23:57 -0400

    Initial commit

---
+++ app/src/main/java/com/halalface/powermeter2/MainActivity.java
@@ -0,0 +1,104 @@
+package com.halalface.powermeter2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        return nav(menuItem);
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public boolean nav(MenuItem menuItem){
+        // set item as selected to persist highlight
+        menuItem.setChecked(true);
+        // close drawer when item is tapped
+        drawerLayout.closeDrawers();
+        Intent intent;
+        intent = new Intent(getApplicationContext(), MainActivity.class);
+        switch (menuItem.getItemId()) {
+            case R.id.home:
+                //System.out.println("MENU ITEM CLICKED " +"home" );
+                break;
+            case R.id.update_add:
+                //System.out.println("MENU ITEM CLICKED " +"update_add");
+                intent = new Intent(getApplicationContext(),  MainActivity.class);
+                break;
+
+            case R.id.view_data:
+                //System.out.println("MENU ITEM CLICKED " +"view_data");
+                intent = new Intent(getApplicationContext(),  MainActivity.class);
+                break;
+
+            case R.id.edit:
+                //System.out.println("MENU ITEM CLICKED " +"edit");
+                intent = new Intent(getApplicationContext(),  MainActivity.class);
+                break;
+
+            case R.id.pr:
+                //System.out.println("MENU ITEM CLICKED " +"pr");
+                intent = new Intent(getApplicationContext(),  MainActivity.class);
+                break;
+            case R.id.atributions:
+                //System.out.println("MENU ITEM CLICKED " +"pr");
+                intent = new Intent(getApplicationContext(),  MainActivity.class);
+                break;
+        }
+        startActivity(intent);
+        return true;
+    }
+}
